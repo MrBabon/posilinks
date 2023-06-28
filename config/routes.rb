8238933d3d9252cb8b1e8efe86a3495377eadd88
william daly <w.daly0200@gmail.com>
2023-06-28 15:21:43 +0200

    banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
