4e54c485f3bcbb2f9646803ced56bd996c675e94
Marion Poirie <marionpoirie1996@gmail.com>
2023-06-29 16:43:19 +0200

    homepage + events/show

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_28_124614) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
