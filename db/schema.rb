e272a0b296ee20c4e4a275a79cdb2bd1430de810
Christofvb <christof.vanberendonck@hotmail.com>
2023-06-29 17:37:59 +0200

    ex

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_27_140551) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
