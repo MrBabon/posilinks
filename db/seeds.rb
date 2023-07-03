6caec4d96802d39f21798f59065a559fe6c16cf6
Marion Poirie <marionpoirie1996@gmail.com>
2023-07-03 10:42:03 +0200

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,6 +71,13 @@ nettoyage = Event.create!(
   date: "2023-07-02",
   organization: wwf
 )
+
+article1 = Article.create!(
+  title: "Protections à prendre avant l'événement",
+  content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+  event: nettoyage
+)
+
 collecte1 = Event.create!(
   address: "606, Bd du mercantour, 06200 Nice",
   title: "collecte de denrées alimentaire",
@@ -78,6 +85,7 @@ collecte1 = Event.create!(
   date: "2023-07-12",
   organization: wwf
 )
+
 collecte2 = Event.create!(
   address: "461 ch des vignes, 83440 Montauroux",
   title: "collecte de denrées alimentaire",
@@ -85,6 +93,7 @@ collecte2 = Event.create!(
   date: "2023-07-12",
   organization: wwf
 )
+
 collecte3 = Event.create!(
   address: "43 av auguste renoir, 06800 Cagne-sur-mer",
   title: "collecte de denrées alimentaire",
@@ -92,11 +101,7 @@ collecte3 = Event.create!(
   date: "2023-07-12",
   organization: wwf
 )
-article1 = Article.create!(
-  title: "Article 1",
-  content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
-  event: nettoyage
-)
+
 collecte = Event.create!(
   address: "606, Bd du mercantour, 06200 Nice",
   title: "collecte de denrées alimentaire",
