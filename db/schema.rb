c683865a9810439b95bbbba57b32dba8a93002a9
Christofvb <christof.vanberendonck@hotmail.com>
2023-07-05 15:05:28 +0200

    ex

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_124614) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nom d'utilisateur"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
